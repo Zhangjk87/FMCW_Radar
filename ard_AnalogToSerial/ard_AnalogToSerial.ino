b114a26519ade31bbe8a67b713d93980016deb06
Adam Zachar <zachara@seas.upenn.edu>
2016-10-30 14:16:28 -0400

    Convert demo analog read file to test case

--- ard_AnalogToSerial/ard_AnalogToSerial.ino
+++ ard_AnalogToSerial/ard_AnalogToSerial.ino
@@ -41,13 +41,15 @@ void loop() {
   analogWrite(analogOutPin, outputValue);
 
   // print the results to the serial monitor:
-  Serial.print("sensor = " );
-  Serial.print(sensorValue);
-  Serial.print("\t output = ");
-  Serial.println(outputValue);
+  //Serial.print("sensor = " );
+  
+  Serial.print(sensorValue + "\n");
+
+  //Serial.print("\t output = ");
+  //Serial.println(outputValue);
 
   // wait 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle
   // after the last reading:
-  delay(2);
+  delay(.02);
 }
